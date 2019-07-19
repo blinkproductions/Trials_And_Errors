d02eeebb1b769dff40d1677caaa5edce0f55f8ca
oO-Blink-Oo <alvarezian23@gmail.com>
2019-07-19 15:27:05 -0700

    UPROPERTY Macro; trying out different parameters

---
+++ FirstProject/Source/FirstProject/Private/Floater.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Floater.h"
+
+// Sets default values
+AFloater::AFloater()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("MyMesh"));
+	SetRootComponent(StaticMesh);
+
+	InitialLocation = FVector(0.f);
+	PlacedLocation = FVector(0.f);
+	bInitializeFloaterLocations = false;
+}
+
+// Called when the game starts or when spawned
+void AFloater::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	PlacedLocation = GetActorLocation();
+
+	if (bInitializeFloaterLocations)
+	{
+		SetActorLocation(InitialLocation);
+	}
+}
+
+// Called every frame
+void AFloater::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
